b38ea2e20171f820293a4b71609be1174087925d
NadavWix <nadav@wix.com>
2024-07-16 12:51:18 +0300

    updated app def

--- _codux/app.app-def.tsx
+++ _codux/app.app-def.tsx
@@ -1,27 +1,24 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React, { useMemo } from 'react';
 import {
     defineApp,
-    type IReactAppManifest,
+    type IAppManifest,
     type IResults,
     type RouteInfo,
-} from '@wixc3/react-board';
+    type RequireModule,
+    IReactAppProps,
+    PageInfo,
+} from '@wixc3/app-core';
 import { Outlet } from '@remix-run/react';
 import { createRemixStub } from '@remix-run/testing';
 import { LoaderFunction } from '@remix-run/node';
-import './app.global.css';
 type RouteObject = Parameters<typeof createRemixStub>[0][0];
 
 const createEl = React.createElement;
-const manifestToRouter = (
-    manifest: IReactAppManifest<RouteExtraInfo>,
-    requireModule: (filePath: string) => {
-        moduleResults: Promise<IResults<unknown>>;
-        dispose: () => void;
-    }
-) => {
+const manifestToRouter = (manifest: IAppManifest<RouteExtraInfo>, requireModule: RequireModule) => {
     const Router = createRemixStub(
         manifest.routes.map((route) => {
-            const { Component, loader } = lazyCompAndLoader(
+            const { Component, loader } = getLazyCompAndLoader(
                 route.pageModule,
                 route.pageExportName || 'default',
                 requireModule
@@ -45,7 +42,7 @@ const manifestToRouter = (
             };
             if (route.parentLayouts) {
                 for (const layout of route.parentLayouts.reverse()) {
-                    const { Component, loader } = lazyCompAndLoader(
+                    const { Component, loader } = getLazyCompAndLoader(
                         layout.layoutModule,
                         layout.layoutExportName || 'default',
                         requireModule,
@@ -66,42 +63,78 @@ const manifestToRouter = (
     return Router;
 };
 
+const loadedModules = new Map<string, ReturnType<typeof lazyCompAndLoader>>();
+export const getLazyCompAndLoader = (
+    filePath: string,
+    compExportName: string,
+    requireModule: RequireModule,
+    wrapWithOutlet = false
+) => {
+    const key = `${filePath}:${compExportName}`;
+    let module = loadedModules.get(key);
+    if (!module) {
+        module = lazyCompAndLoader(filePath, compExportName, requireModule, wrapWithOutlet);
+        loadedModules.set(key, module);
+    }
+    return module;
+};
+
 function lazyCompAndLoader<ExportName extends string>(
     filePath: string,
     compExportName: ExportName,
-    requireModule: (filePath: string) => {
-        moduleResults: Promise<IResults<unknown>>;
-        dispose: () => void;
-    },
+    requireModule: RequireModule,
     wrapWithOutlet = false
 ) {
     const Component = React.lazy(async () => {
-        const { moduleResults } = requireModule(filePath);
-        const module = await moduleResults;
-        const moduleWithComp = module.results as {
-            [key in ExportName]: React.ComponentType<{ children?: React.ReactNode }>;
-        };
+        let updateModule: ((newModule: IResults<unknown>) => void) | undefined;
+        const { moduleResults } = requireModule(filePath, (newResults) => {
+            updateModule?.(newResults);
+        });
+        const initialyLoadedModule = await moduleResults;
 
-        const Page = moduleWithComp[compExportName];
-        if (!Page) {
-            return {
-                default: () => (
+        const PageComp = () => {
+            // const [updatedModule, setModule] =
+            //     React.useState<IResults<unknown>>(initialyLoadedModule);
+            // const isDisposed = React.useRef(false);
+            // React.useEffect(() => {
+            //     return () => {
+            //         isDisposed.current = true;
+            //     };
+            // }, []);
+            // const updateModuleIfNotDisposed = (newModule: IResults<unknown>) => {
+            //     if (!isDisposed.current) {
+            //         setModule(newModule);
+            //     }
+            // };
+            // updateModule = updateModuleIfNotDisposed;
+            const current = initialyLoadedModule || module;
+            if (current.errorMessage) {
+                return <div>{current.errorMessage}</div>;
+            }
+            const Page = (
+                initialyLoadedModule.results as {
+                    [key in ExportName]: React.ComponentType<{ children?: React.ReactNode }>;
+                }
+            )[compExportName];
+
+            if (!Page) {
+                return (
                     <div>
                         {compExportName} export not found at {filePath}{' '}
                     </div>
-                ),
+                );
+            }
+            if (wrapWithOutlet) {
                 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            } as any;
-        }
-        if (wrapWithOutlet) {
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            const Layout = Page as any;
-            return {
-                default: () => createEl(Layout, {}, [createEl(Outlet)]),
-            };
-        }
+                const Layout = Page as any;
+                return createEl(Layout, {}, [createEl(Outlet)]);
+            }
+            return createEl(Page);
+        };
         return {
-            default: Page,
+            default: () => {
+                return createEl(PageComp);
+            },
         };
     });
 
@@ -125,8 +158,19 @@ interface RouteExtraInfo {
     rendereredRoutes: string[];
 }
 
+const routePathId = (path: RouteInfo['path']) => {
+    return path
+        .map((part) => {
+            if (part.kind === 'static') {
+                return part.text;
+            }
+            return `$$$`;
+        })
+        .join('/');
+};
+
 export default defineApp<RouteExtraInfo>({
-    App: ({ manifest, requireModule, uri }) => {
+    App: ({ manifest, requireModule, uri }: IReactAppProps<RouteExtraInfo>) => {
         const App = useMemo(
             () => manifestToRouter(manifest, requireModule),
             [manifest, requireModule]
@@ -146,21 +190,43 @@ export default defineApp<RouteExtraInfo>({
             </>
         );
     },
-    getNewPageInfo({ fs, wantedPath }) {
-        const routeDir = fs.join(__dirname, './example-app/routes');
+    getNewPageInfo({ fs, wantedPath, manifest }) {
+        const routeDir = fs.join(__dirname, '../app/routes');
         const varNames = new Set<string>();
+
+        if (wantedPath.length === 0 && manifest.homeRoute) {
+            return {
+                isValid: false,
+                errorMessage: 'Home route already exists at ' + manifest.homeRoute.pageModule,
+                pageModule: manifest.homeRoute.pageModule,
+                newPageSourceCode: '',
+            };
+        }
+        const wantedPathId = routePathId(wantedPath);
+        const existingRoute = manifest.routes.find(
+            (route) => routePathId(route.path) === wantedPathId
+        );
+        if (existingRoute) {
+            return {
+                isValid: false,
+                errorMessage: 'Route already exists at file path: ' + existingRoute.pageModule,
+                pageModule: existingRoute.pageModule,
+                newPageSourceCode: '',
+            };
+        }
+
         const pageFileName = wantedPath
             .map((part) => {
                 if (part.kind === 'static') {
                     return part.text;
                 }
                 varNames.add(part.name);
-                return `{${part.name}}`;
+                return `$${part.name}`;
             })
             .join('.');
         const pageName = toCamelCase(pageFileName);
-        const pagePath = fs.join(routeDir, pageFileName + '.tsx');
-        const contents =
+        const pageModule = fs.join(routeDir, pageFileName + '.tsx');
+        const newPageSourceCode =
             varNames.size === 0
                 ? `
 import React from 'react';
@@ -190,54 +256,51 @@ export default ${pageName};
                 `;
 
         return {
-            contents,
-            pagePath,
+            isValid: true,
+            error: '',
+            newPageSourceCode,
+            pageModule,
         };
     },
-    async prepareApp({ fs, onAppUpdate }) {
+
+    async prepareApp({ fs, onManifestUpdate }) {
         const rootPath = fs.join(__dirname, '../app/root.tsx');
         const routeDir = fs.join(__dirname, '../app/routes');
-        const route = (
-            path: RouteInfo['path'],
-            pageModule: string,
-            renderedRoutes: string[]
-        ): RouteInfo<RouteExtraInfo> => ({
-            path,
-            pageModule,
-            pageExportName: 'default',
-            parentLayouts: [
-                {
-                    layoutModule: rootPath,
-                    layoutExportName: 'Layout',
-                },
-            ],
-            isPage: true,
-            extraData: { rendereredRoutes: renderedRoutes },
-        });
+
         const compute = () => {
+            const filesInDir = fs.readdirSync(routeDir);
+            const homeRouteModule = filesInDir.find((name) => name === '_index.tsx');
+            const errorRouteModule = filesInDir.find((name) => name === 'errors.tsx');
+
             const routeDirChildren = fs.readdirSync(routeDir).map((name) => {
                 const fullPath = fs.join(routeDir, name);
                 const isFile = fs.statSync(fullPath).isFile();
                 if (isFile && name.endsWith('.tsx')) {
-                    if (name === '_index.tsx') {
-                        return route([], fullPath, [rootPath, fullPath]);
+                    if (name === '_index.tsx' || name === 'errors.tsx') {
+                        return null;
                     }
                     const fileName = fs.basename(name, '.tsx');
                     const routeParts = fileName.split('.');
 
-                    return route(
-                        routeParts.map((p) => {
-                            if (p.startsWith('$')) {
+                    return aRoute(
+                        rootPath,
+                        routeParts
+                            .map((p) => {
+                                if (p.startsWith('$')) {
+                                    return {
+                                        kind: 'dynamic' as const,
+                                        name: p.slice(1, p.length),
+                                    };
+                                }
+                                if (p === '_index') {
+                                    return null;
+                                }
                                 return {
-                                    kind: 'dynamic' as const,
-                                    name: p.slice(1, -1),
+                                    kind: 'static' as const,
+                                    text: p,
                                 };
-                            }
-                            return {
-                                kind: 'static' as const,
-                                text: p,
-                            };
-                        }),
+                            })
+                            .filter((p): p is RouteInfo['path'][0] => !!p),
                         fullPath,
                         [rootPath, fullPath]
                     );
@@ -245,9 +308,17 @@ export default ${pageName};
                 return null;
             });
             const routes = routeDirChildren.filter((r): r is RouteInfo<RouteExtraInfo> => !!r);
-            return routes.sort(
-                (a, b) => a.path.length - b.path.length
-            ) as RouteInfo<RouteExtraInfo>[];
+            return {
+                routes: routes.sort(
+                    (a, b) => a.path.length - b.path.length
+                ) as RouteInfo<RouteExtraInfo>[],
+                homeRoute: homeRouteModule
+                    ? aRoute(rootPath, [], fs.join(routeDir, homeRouteModule), [])
+                    : undefined,
+                errorRoute: errorRouteModule
+                    ? aRoute(rootPath, [], fs.join(routeDir, errorRouteModule), [])
+                    : undefined,
+            };
         };
 
         const watcher = fs.watch(routeDir, {
@@ -255,21 +326,16 @@ export default ${pageName};
         });
 
         watcher.on('change', () => {
-            onAppUpdate({
-                routes: compute(),
-            });
+            onManifestUpdate(compute());
         });
 
         return {
             dispose() {
                 watcher.close();
             },
-            manifest: {
-                routes: compute(),
-            },
+            manifest: compute(),
         };
     },
-    bookmarks: ['/example-app', '/example-app/about', '/example-app/home', '/example-app/other'],
 });
 function capitalizeFirstLetter(val: string): string {
     return val.length === 0 ? val : val.charAt(0).toUpperCase() + val.slice(1);
@@ -282,3 +348,29 @@ function toCamelCase(str: string): string {
         );
     return words.join('');
 }
+
+const aRoute = (
+    rootPath: string,
+    path: RouteInfo['path'],
+    pageModule: string,
+    renderedRoutes: string[]
+): RouteInfo<RouteExtraInfo> => ({
+    path,
+    ...aPage(rootPath, pageModule, renderedRoutes),
+});
+
+const aPage = (
+    rootPath: string,
+    pageModule: string,
+    renderedRoutes: string[]
+): PageInfo<RouteExtraInfo> => ({
+    pageModule,
+    pageExportName: 'default',
+    parentLayouts: [
+        {
+            layoutModule: rootPath,
+            layoutExportName: 'Layout',
+        },
+    ],
+    extraData: { rendereredRoutes: renderedRoutes },
+});
